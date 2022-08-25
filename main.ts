6c8992cf815fbcdf43935a000f503ac056fa433e
Ivan Sanchez Leyva <deepman42@protonmail.com>
2022-08-25 21:57:44 +0200

    typing number and recicling with function

---
+++ main.ts
@@ -0,0 +1,18 @@
+const a : number = 7
+
+interface MyInterface {
+    x: number;
+    y: number;
+    z?: number;
+}
+
+const int: MyInterface = {
+    x: 5 ,
+    y: 9
+}
+const int2 : MyInterface = {
+    x: 8,
+    y: 14
+}
+
+// everething good with type

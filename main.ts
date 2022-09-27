5889d6e8c273e8d5a52abfdbade30fe373c7f83f
Ivan Sanchez Leyva <deepman42@protonmail.com>
2022-09-28 00:56:22 +0200

    practices file

--- main.ts
+++ main.ts
@@ -35,3 +35,31 @@ let maybeNum: number | string | boolean | AddFunc = 5;
 
 maybeNum = "hello";
 maybeNum = false;
+
+interface Dog {
+    bark:string;
+}
+
+interface Cat {
+    purr:string;
+}
+
+type DogCat = Dog & Cat | string | boolean | AddFunc;
+
+let dogCat: DogCat = {
+    bark: "bark!",
+    purr: "purr!"
+
+}
+
+if (typeof dogCat === "string") {
+    console.log("you are a string")
+}
+
+// casting
+
+add (dogCat as any, dogCat as any)
+
+
+
+

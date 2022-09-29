4621bffa1ae72f873176656def04014f68dd4ac7
Ivan Sanchez Leyva <deepman42@protonmail.com>
2022-09-29 15:08:20 +0200

    creating a default props for

--- components-practices/src/App.tsx
+++ components-practices/src/App.tsx
@@ -9,13 +9,26 @@ function HeadingwithContext ({children}: {children: ReactNode}): ReactElement {
     return <h1>{children}</h1>
     }
 
+//Default properties
 
+const defaultContainerProps = {
+    heading: <h1> im a headin 1</h1>
+};
+
+function Container ({heading, children}: {children: ReactNode } & typeof defaultContainerProps ): ReactElement {
+  return <div><h1>{heading}</h1>{children}</div>
+  }
+
+  Container.defaultProps = defaultContainerProps
 
 function App() {
   return (
     <div >
     <Headings title="hello world"></Headings>
      <HeadingwithContext><strong>lol</strong></HeadingwithContext> 
+     <Container>
+      foo
+     </Container>
     </div>
   );
 }

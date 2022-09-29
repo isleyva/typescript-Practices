342e332bad6e5db18e39004b6d079b6d4b09cbfc
Ivan Sanchez Leyva <deepman42@protonmail.com>
2022-09-29 16:19:34 +0200

    prettier

--- components-practices/src/App.tsx
+++ components-practices/src/App.tsx
@@ -1,67 +1,72 @@
-import React, { ReactNode, ReactElement } from 'react';
+import React, { ReactNode, ReactElement } from "react";
 
 // type for a defined prop
 
-function Headings({title}: {title: string}){
-  return <h1>{title}</h1>
+function Headings({ title }: { title: string }) {
+  return <h1>{title}</h1>;
 }
 
 //Type for Childrens
 
-function HeadingwithContext ({children}: {children: ReactNode}): ReactElement {
-    return <h1>{children}</h1>
-    }
+function HeadingwithContext({
+  children,
+}: {
+  children: ReactNode;
+}): ReactElement {
+  return <h1>{children}</h1>;
+}
 
 //Default properties
 
 const defaultContainerProps = {
-    heading: <h3> im a headin 1</h3>
+  heading: <h3> im a headin 1</h3>,
 };
 
-type ContainerProps = {children: ReactNode } & typeof defaultContainerProps;
+type ContainerProps = { children: ReactNode } & typeof defaultContainerProps;
 
-function Container ({heading, children}: ContainerProps) : ReactElement {
-  return <div><h1>{heading}</h1>{children}</div>
-  }
+function Container({ heading, children }: ContainerProps): ReactElement {
+  return (
+    <div>
+      <h1>{heading}</h1>
+      {children}
+    </div>
+  );
+}
 // Props fuction
-  Container.defaultProps = defaultContainerProps
+Container.defaultProps = defaultContainerProps;
 
-  //Functional props
+//Functional props
 
-  function TextWithNumber ({
-    children,
-    header
-  }:{
-    children: (num: number) => ReactNode
-    header:(num: number) => ReactNode
-  }) {
-    const [state, stateSet] = React.useState <number> (0)
-    return (
+function TextWithNumber({
+  children,
+  header,
+}: {
+  children: (num: number) => ReactNode;
+  header: (num: number) => ReactNode;
+}) {
+  const [state, stateSet] = React.useState<number>(0);
+  return (
+    <div>
+      <h3>{header(state)}</h3>
+      <div>{children(state)}</div>
       <div>
-        <h3>{header(state)}</h3>
-        <div>
-          {children (state)}
-        </div>
-        <div>
-          <button onClick={() => stateSet(state + 1)}>add</button>
-        </div>
+        <button onClick={() => stateSet(state + 1)}>add</button>
       </div>
-    )
-  
-  }
-
-  
-
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div >
-    <Headings title="hello world"></Headings>
-     <HeadingwithContext><strong>lol</strong></HeadingwithContext> 
-     <Container>
-      foo
-     </Container>
-     <TextWithNumber header={(num: number) => <div>header {num}</div>}>{(num: number) => <div> the number is {num}</div>}</TextWithNumber>
+    <div>
+      <Headings title="hello world"></Headings>
+      <HeadingwithContext>
+        <strong>lol</strong>
+      </HeadingwithContext>
+      <Container>foo</Container>
+      <TextWithNumber header={(num: number) => <div>header {num}</div>}>
+        {(num: number) => <div> the number is {num}</div>}
+      </TextWithNumber>
     </div>
   );
 }

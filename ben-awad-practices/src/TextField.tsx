37e69f5ec6ef2c4dd3f6b096a88c67968e3de27b
Ivan Sanchez Leyva <deepman42@protonmail.com>
2022-10-02 17:23:54 +0200

    useStae type number

--- ben-awad-practices/src/TextField.tsx
+++ ben-awad-practices/src/TextField.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import {useState} from "react"
 
 interface Props {
     text: string;
-   
+
 }
 
 const TextField: React.FC <Props> = () => {
-  return (
+  const [count, setCount] = useState(1)
+  // while you use useState it dectects type into as a number.
+  setCount(count + 1);
+ 
+    return (
     <div><input/></div>
   )
 }
